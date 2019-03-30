6130f9228a8d5573190056ab45dd0b6ab050db5f
Faceless <48881132+faceythefaceless@users.noreply.github.com>
2019-03-30 12:41:10 +0545

    Update disbot.py

--- disbot.py
+++ disbot.py
@@ -197,11 +197,5 @@ async def kiss(user):
 async def punch(user):
           """ It Punches A User"""
           await bot.say(" You Got Punched In Face! "+ user + " :punch: :dizzy_face:  ")
-          
-@bot.command()
-async def love(user):
-          """It says I love you to the user"""
-          await bot.say(" I Love You "+ user + " :heart: "
-          
-                       
+                           
 bot.run(os.environ['BOT_TOKEN'])
