7de08f3cdee5c70782ed9cf0ce864c11c5219102
Faceless <48881132+faceythefaceless@users.noreply.github.com>
2019-03-30 08:48:02 +0545

    Update disbot.py

--- disbot.py
+++ disbot.py
@@ -174,6 +174,6 @@ async def helpme():
 
 @bot.command()
 async def greet():
-          await bot.say("Hello , {mention.user} :hugging:") 
+          await bot.say("Hello , member.mention :hugging:") 
 
 bot.run(os.environ['BOT_TOKEN'])
