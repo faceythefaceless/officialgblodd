6dee3d55c5ac0101e9fce1d263c3d9d4e7012c72
Faceless <48881132+faceythefaceless@users.noreply.github.com>
2019-03-30 09:43:18 +0545

    Update disbot.py

--- disbot.py
+++ disbot.py
@@ -189,25 +189,28 @@ async def jiugare(user):
         await bot.say("Jiu Gare Hai!, La Khai Ashirwad Pani Dinu "+ user + " :heart: ")
 
 @bot.command()
-async def qball():
-          await bot.say(random.choice)[ "It is certain :8ball:",
-                                          "It is decidedly so :8ball:",
-                                          "Without a doubt :8ball:",
-                                          "Yes, definitely :8ball:",
-                                          "You may rely on it :8ball:",
-                                          "As I see it, yes :8ball:",
-                                          "Most likely :8ball:",
-                                          "Outlook good :8ball:",
-                                          "Signs point to yes :8ball:",
-                                          "Reply hazy try again :8ball:",
-                                          "Ask again later :8ball:",
-                                          "Better not tell you now :8ball:",
-                                          "Cannot predict now :8ball:",
-                                          "Concentrate and ask again :8ball:",
-                                          "Don't count on it :8ball:",
-                                          "My reply is no :8ball:",
-                                          "My sources say no :8ball:",
-                                          "Outlook not so good :8ball:",
-                                          "Very doubtful :8ball:"]
+async def qball():         
+          await bot.say(message.channel, random.choice(["""It is certain :8ball:",
+                          "It is decidedly so :8ball:",
+                          "Without a doubt :8ball:",
+                          "Yes, definitely :8ball:",
+                          "You may rely on it :8ball:",
+                          "As I see it, yes :8ball:",
+                          "Most likely :8ball:",
+                          "Outlook good :8ball:",
+                          "Yes :8ball:",
+                          "Signs point to yes :8ball:",
+                          "Reply hazy try again :8ball:",
+                          "Ask again later :8ball:",
+                          "Better not tell you now :8ball:",
+                          "Cannot predict now :8ball:",
+                          "Concentrate and ask again :8ball:",
+                          "Don't count on it :8ball:",
+                          "My reply is no :8ball:",
+                          "My sources say no :8ball:",
+                          "Outlook not so good :8ball:"""]))
+                           "Very doubtful :8ball:""")                                            
+                                                                     
+                                                                     
 
 bot.run(os.environ['BOT_TOKEN'])
