262d48539fd8442293b7dcc471afbf81c9d45808
Faceless <48881132+faceythefaceless@users.noreply.github.com>
2019-03-31 20:09:23 +0545

    Update disbot.py

--- disbot.py
+++ disbot.py
@@ -231,4 +231,29 @@ async def ashirwad(ctx, member: discord.Member):
     """Gets Ashirwad from A User"""
     await bot.say("{1} Gave Ashirwad to :raised_hand: {0}".format(member.mention, ctx.message.author.mention))
           
+@bot.command(pass_context=True)
+async def 8ball(ctx):
+    """Ask an 8ball Question"""
+    await bot.say(random.choice(["It is certain :8ball:",
+                                 "It is decidedly so :8ball:",
+                                 "Without a doubt :8ball:",
+                                 "Yes, definitely :8ball:",
+                                 "You may rely on it :8ball:",
+                                 "As I see it, yes :8ball:",
+                                 "Most likely :8ball:",
+                                 "Outlook good :8ball:",
+                                 "Yes :8ball:",
+                                 "Signs point to yes :8ball:",
+                                 "Reply hazy try again :8ball:",
+                                 "Ask again later :8ball:",
+                                 "Better not tell you now :8ball:",
+                                 "Cannot predict now :8ball:",
+                                 "Concentrate and ask again :8ball:",
+                                 "Don't count on it :8ball:",
+                                 "My reply is no :8ball:",
+                                 "My sources say no :8ball:",
+                                 "Outlook not so good :8ball:",
+                                  "Very doubtful :8ball:"]))
+
+          
 bot.run(os.environ['BOT_TOKEN'])
