ea9f4f0f567030fcc4e88c078c0c5a5db40ad12b
Faceless <48881132+faceythefaceless@users.noreply.github.com>
2019-03-31 17:19:03 +0545

    Update disbot.py

--- disbot.py
+++ disbot.py
@@ -216,7 +216,7 @@ async def love(ctx, member: discord.Member):
     """Says i love you to someone"""
     await bot.say("{1} Loves {0}! :heart:".format(member.mention, ctx.message.author.mention))
           
-@bot.command()
+@bot.command(pass_context=True)
 async def marry(ctx, member: discord.Member):
     """Get Married to Someone"""
     await bot.say("{1} Got Married With :ring: {0}! :heart:".format(member.mention, ctx.message.author.mention)) 
